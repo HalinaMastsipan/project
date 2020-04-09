19145a8d9d8968325e253652c9567bbee7bbfe43
Halina Mastsipan <h.mastsipan@godeltech.com>
2020-04-09 15:40:05 +0300

    init commit

---
+++ FirstProject/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProject
+{
+    class Array
+    {
+        public static void Main (String [] args)
+        {
+            int a = enteredValue();
+            Console.WriteLine(countValue(a));
+
+            int enteredValue()
+            {
+                Console.WriteLine("Insert some number");
+                string N = Console.ReadLine();
+                int value;
+                while (!int.TryParse(N, out value) || !(value <= 100))
+                {
+                    Console.WriteLine("error");
+                    N = Console.ReadLine();
+                }
+                return value;
+            }
+
+            string countValue(int value)
+            {
+                int result = 0;
+                for (int i = 1; i <= value; i++)
+                {
+                    result = result + i;
+                }
+                return result.ToString();
+            }
+        }
+
+
+    }
+}
